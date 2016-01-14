b64b434045db8aa52664abed067abd6ed1ff3c2e
gfishel <gfishel@oberlin.edu>
2016-01-14 13:03:03 -0500

    Manager Fixes
    Removed previous part of management.cpp

--- management.cpp
+++ management.cpp
@@ -7,29 +7,13 @@
 #include <map>
 #include "share.h"
 
-int main(int argc, char *argv[]){
-    // Hash map with <Symbol, Share>
-    //std::map<std::string, Share> investments;
+class Manager {
+    public:
 
-    float funds;
+    private:
 }
 
-/*
-* Buy Shares
-* > Symbol of stock
-* > Volume of shares
-* > Price of shares
-* < Integer indicating success (0)
-*/
-int buyShares( string sym, int vol, float cost ){
-    if ( vol * cost > funds ) {
-        return 1
-    } else{
-        // TODO: get time, date, and store share in hash map.
-        time=0;
-        date=0;
-        Share invested_shares(cost,vol,time,date);
-        funds = funds - vol*cost;
-        return 0
-    }
+
+int main(int argc, char *argv[]){
+
 }
