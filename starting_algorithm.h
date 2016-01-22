3f7c223404bb4650e534252d4bbe967011c9614e
gfishel <gfishel@oberlin.edu>
2016-01-22 16:39:31 -0500

    More Algorithm to Love
    Fleshed out more of the algorithm, including: writing the main loop,
    declaring more necessary functions, writing some helper functions (such
    as consider_sell and consider_buy).

--- starting_algorithm.h
+++ starting_algorithm.h
@@ -19,11 +19,23 @@ class StartingAlgorithm : public StockAlgorithm {
             const std::map<std::string, Share> portfolio,
             long time)
         {
+            // TODO: double check this algorithm
             std::vector<Share>* buy_and_sell = new std::vector<Share>[2];
             std::vector<Share> buy;
             std::vector<Share> sell;
-            // TODO: update metric values for all new information stocks
-            // TODO: compile buy and sell orders based on the new metric
+            for (std::map<std::string, float>::const_iterator iter = prices.begin(); iter != prices.end(); ++iter) {
+                std::string symbol = iter->first;
+                float price = prices.at(symbol);
+                this.update_metric( symbol_records_.at(symbol), current_funds, price, portfolio, time);
+                if( this.consider_sell_share( symbol_records_.at(symbol) ) ) {
+                    Share sellable_share = portfolio.at(symbol);
+                    sell.push_back( sellable_share );
+                } else if( this.consider_buy_share( symbol_records_.at(symbol) ) ) {
+                    int recommended_volume = this.recommend_buy_volume( symbol_records_.at(symbol), current_funds );
+                    Share buy_share( symbol, price, recommended_volume, time);
+                    buy.push_back( buy_share );
+                }
+            }
             buy_and_sell[0] = buy;
             buy_and_sell[1] = sell;
             return buy_and_sell;
@@ -34,6 +46,8 @@ class StartingAlgorithm : public StockAlgorithm {
         std::map<std::string, Market_Record> symbol_records_;
         const double metric_hard_buy_point_;
         const double metric_hard_sell_point_;
+        const double buy_price_volume_ratio_;
+        
         // TODO:
         void update_metric(
             Market_Record record,
@@ -41,7 +55,17 @@ class StartingAlgorithm : public StockAlgorithm {
             float price_update,
             Share portfolio,
             long time);
-        bool consider_sell_share(Market_Record share_record);
-        bool consider_buy_share(Market_Record share_record);
+        float calculate_volatility(Market_Record record);
+        float calculate_growth(Market_Record record);
+        int   recommend_buy_volume( Share buy_share, float current_funds );
+
+        bool  consider_sell_share(Market_Record share_record){
+            if( share_record.get_current_value() > metric_hard_buy_point_ ) return true;
+            else return false;
+        }
+        bool  consider_buy_share(Market_Record share_record){
+            if( share_record.get_current_value() < metric_hard_sell_point_ ) return true;
+            else return false;
+        }
 
 };
